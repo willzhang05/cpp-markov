22c5f6d41bc3dadb1d52dd18e5055afaebe6be87
William Zhang <willzhang05@gmail.com>
2016-04-29 19:45:28 -0400

    Updated and added mapping

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <vector>
-#include <map>
 #include "markov.h"
 
 using std::cout;
@@ -13,50 +13,64 @@ using std::vector;
 using std::ifstream;
 using std::stringstream;
 
+markov::markov() {}
 markov::markov(vector<string> src) {
-    
+    for (int i = 0; i < (int)src.size() - 1; i++) {
+        vector<string> key;
+        key.push_back(src[i]);
+        key.push_back(src[i + 1]);
+        vector<string> values;
+        values.push_back(src[i + 2]);
+        if (map.find(key) == map.end())
+            map[key] = values;
+        else {
+            map[key].push_back(src[i + 2]);
+        }
+        cout << ind_to_string(key, map[key]) << endl;
+    }
 }
 
 string markov::gen_chain(int lim) {
+    string buff;
+    if (lim < 0) {
+        string cur_val;
+        while (cur_val != "") {
+        }
+    }
+
     return "";
 }
 
-void markov::update_val(vector<string>, string val) {
-}
-
-void markov::contains_key(vector<string>) {
-}
-
-vector<string> &split(string &s, char delim, vector<string> &elems) {
-    stringstream ss(s);
-    string item;
-    while (getline(ss, item, delim)) {
-        elems.push_back(item);
+string markov::ind_to_string(vector<string> key, vector<string> val) {
+    string buff("[" + key[0] + ", " + key[1] + "] -> [");
+    for (int i = 0; i < (int)val.size(); i++) {
+        buff += val[i] + ", ";
     }
-    return elems;
+    return buff.substr(0, buff.size() - 2) + "]";
 }
 
 int main(int argc, char *argv[]) {
-    if(argc < 2) {
-        cout << "Error: Wrong number of arguments" << endl << "Usage: ./main <filename> <length>" << endl;
+    if (argc < 2) {
+        cout << "Error: Wrong number of arguments" << endl
+             << "Usage: ./main <filename> <length>" << endl;
         return 1;
-    } else if(argc == 3) {
-        string filename = string(argv[1]);
-        string line;
-        
-        int max_len = std::stoi(argv[2]);
-        ifstream infile(filename);
-        vector<string> text;
-        while (getline(infile, line)) {
-            split(line, ' ', text);
-        }
-        for (int i = 0; i < text.size(); i++) {
-            cout << text.at(i) << endl;
-        }
-
-    } else {
-        
     }
-    
+    int max_len = argc == 3 ? std::stoi(argv[2]) : -1;
+    string filename = string(argv[1]);
+    string line;
+    ifstream infile(filename);
+    vector<string> text;
+    while (getline(infile, line, ' ')) {
+        text.push_back(line);
+        if (line.at(line.size() - 1) == '.') {
+            text.push_back("");
+            text.push_back("");
+        }
+    }
+    for (int i = 0; i < (int)text.size(); i++) {
+        cout << text.at(i) << endl;
+    }
+    markov::markov m(text);
+    m.gen_chain(max_len);
     return 0;
 }
--- markov.h
+++ markov.h
@@ -1,11 +1,20 @@
+#ifndef MARKOV_HEADER
+#define MARKOV_HEADER
+
+#include <vector>
+#include <string>
+#include <map>
+
 class markov {
    private:
     std::map<std::vector<std::string>, std::vector<std::string>> map;
 
    public:
+    markov();
     markov(std::vector<std::string> src);
 
     std::string gen_chain(int lim);
-    void update_val(std::vector<std::string>, std::string val);
-    void contains_key(std::vector<std::string>);
+    std::string ind_to_string(std::vector<std::string> key, std::vector<std::string> val);
 };
+
+#endif
